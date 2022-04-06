0917f037246ef355cdce103c43f005a1b0f3ca1f
dihia0902 <dihia.azzedine@gmail.com>
2022-04-06 09:39:45 +0200

    postman

---
+++ src/main/java/fr/univrouen/rss22SB1/controllers/PostController.java
@@ -0,0 +1,15 @@
+package fr.univrouen.rss22SB1.controllers;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class PostController {
+	@RequestMapping(value = "/testpost", method = RequestMethod.POST,
+			consumes = "application/xml")
+	public String postTest(@RequestBody String flux) {
+		return ("<result><response>Message reçu : </response>"
+				+ flux + "</result>");
+	}
+
+}
--- src/main/java/fr/univrouen/rss22SB1/controllers/RSSController.java
+++ src/main/java/fr/univrouen/rss22SB1/controllers/RSSController.java
@@ -1,6 +1,7 @@
 package fr.univrouen.rss22SB1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RSSController {
@@ -15,8 +16,14 @@ public class RSSController {
          
         }
         @GetMapping("/guid")
-        public String getRSSinXML() {
-            return "Détail du contenu RSS demandé";
+        public String getRSSinXML(
+        		@RequestParam(value = "guid") String texte) {
+        		return ("Détail du flux RSS demandé : " + texte);
+        		}
+        @GetMapping("/test")
+        public String getTestRSSinXML( 
+        		@RequestParam(value = "nb") int nb, @RequestParam(value="search")  String titre) {
+        	return("Test :" + " " + "guid  =" + nb  + " " +  "titre = " + titre );
         }
-        }
+ }
 
